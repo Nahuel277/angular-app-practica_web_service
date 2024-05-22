5eb89c4cc96fcdb2a21ab9c2f36ce47e54c7223e
Lautaro Nahuel <lautaromoyata.lm@gmail.com>
2024-05-22 17:14:20 -0300

    Modificacion en el punto a agregandole el boton para copiar y el punto c agregandole una imagen

--- src/app/website/components/punto-a/punto-a.component.ts
+++ src/app/website/components/punto-a/punto-a.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateService } from '../../../core/service/translate.service';
 import { error } from 'node:console';
@@ -14,24 +14,40 @@ import { error } from 'node:console';
 })
 export class PuntoAComponent implements OnInit{
 
-  texto: string = '';
-  source: string = '';
-  target: string = '';
-  textoTraducido: string = '';
+  translateForm: FormGroup;
+  translatedText: string = '';
+  languages = [
+    { code: 'en', name: 'English' },
+    { code: 'fr', name: 'French' },
+    { code: 'it', name: 'Italian' },
+    { code: 'es', name: 'Spanish' }
+  ];
 
-  constructor(public translateService: TranslateService) { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private translateService: TranslateService) {
+    this.translateForm = this.fb.group({
+      text: [''],
+      sourceLanguage: ['auto'],
+      targetLanguage: ['en']
+    });
   }
 
-  traducirTexto(): void {
-    this.translateService.translateText(this.texto, this.target, this.source).subscribe(
-      (result: any) => {
-        this.textoTraducido = result.data.translations[0].translatedText;
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    )
+  ngOnInit(): void { }
+
+  onTranslate() {
+    const formValue = this.translateForm.value;
+    this.translateService.translateText(formValue.text, formValue.targetLanguage, formValue.sourceLanguage).subscribe(response => {
+      this.translatedText = response.data.translations[0].translatedText;
+    }, error => {
+      console.error('Error durante la traducción:', error);
+      this.translatedText = 'La traducción falló. Inténtalo de nuevo.';
+    });
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.translatedText).then(() => {
+      console.log('Texto copiado al portapapeles');
+    }).catch(err => {
+      console.error('Error al copiar al portapapeles', err);
+    });
   }
 }
